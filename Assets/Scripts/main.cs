96ce67e213935171c68f40ef89124bf01804053c
divyaswormakai <makaidivya@gmail.com>
2019-04-02 21:02:10 +0545

    initial commit

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+	public Sprite[] spritebox = new Sprite[16];
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	  
+	}
+}
